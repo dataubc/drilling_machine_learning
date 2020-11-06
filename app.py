d42bbdbf4022158d57eaf0b48985f7df6caacab4
Salama <dataubc@gmail.com>
2020-11-05 18:30:39 -0700

    adding IFT models

--- app.py
+++ app.py
@@ -1,33 +1,40 @@
-import pandas as pd
-from flask import Flask, jsonify, request
+import numpy as np
+from flask import Flask, request, jsonify, render_template
 import pickle
-
-# load model
-model = pickle.load(open('model.pkl','rb'))
-
-# app
+import pandas as pd
+from joblib import dump, load
 app = Flask(__name__)
+filename = "model.pkl"
 
-# routes
-@app.route('/', methods=['POST'])
+with open(filename, 'rb') as file:
+    model = load(file)
 
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['POST'])
 def predict():
-    # get data
-    data = request.get_json(force=True)
+    '''
+    For rendering results on HTML GUI
+    '''
+    X_test_new = [x.strip() for x in request.form.values()]
+    X_test_new[1] = float(X_test_new[1])
+    X_test_new[2] = float(X_test_new[2])
+    # final_features = [np.array(int_features)]
+    # X_test_new = [x for x in request.form.values()]
 
-    # convert data into dataframe
-    data.update((x, [y]) for x, y in data.items())
-    data_df = pd.DataFrame.from_dict(data)
+    # # print(sgd_clf.predict(X_test_new_count))
+    #
+    ll = [X_test_new]
+    new_data = pd.DataFrame(ll, columns=['Gas', 'Water_content', 'time_minutes'])
+    prediction = model.predict(new_data)[0]
+    output = round(prediction, 2)
 
-    # predictions
-    result = model.predict(data_df)
+    return render_template('index.html', prediction_text='Estimated IFT =  {} mN/m'.format(output))
 
-    # send back to browser
-    output = {'results': int(result[0])}
 
-    # return data
-    return jsonify(results=output)
-
-if __name__ == '__main__':
-    app.run(port = 5000, debug=True)
-
+if __name__ == "__main__":
+    app.run(debug=True)
