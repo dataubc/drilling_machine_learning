cb13a5cb378b54fc53d1467a0f3183442c9acf7a
M. AL <54875834+dataubc@users.noreply.github.com>
2020-11-05 18:52:14 -0700

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import requests
-import json
-url = 'https://drilling2020.herokuapp.com/'
-data = {"Pclass":3, "Age":2, "SibSp":1, "Fare":50}
-response = requests.post(url, json.dumps(data))
-print(response.json())
-
-curl -s -XPOST 'https://drilling2020.herokuapp.com/' -d '{"Pclass":3,"Age":2,"SibSp":1,"Fare":50}' -H 'accept-content: application/json'
